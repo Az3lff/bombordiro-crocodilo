903692bcf83dc18fec021582598f1b6e7420aad4
az3lff <az3lff@gmail.com>
2025-05-21 22:43:14 +0300

    Добавлен блок логических операций

--- src/Entities/block/block-generic/index.tsx
+++ src/Entities/block/block-generic/index.tsx
@@ -77,6 +77,7 @@ const BlocklyComponent = () => {
         contents: [
           { kind: "block", type: "controls_if" },
           { kind: "block", type: "logic_compare" },
+          { kind: "block", type: "logic_operation" },
           { kind: "block", type: "math_number" },
         ],
       },
